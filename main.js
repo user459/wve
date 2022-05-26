024fbdac9236f28530ab18051f90567d76c41c2d
user459 <86778583+user459@users.noreply.github.com>
2022-05-26 18:01:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+function takesnapshot(){
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+});
+}
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/IKrZfr-fS/model.json",model_loaded);
+function model_loaded(){
+console.log("success");
+}
+
